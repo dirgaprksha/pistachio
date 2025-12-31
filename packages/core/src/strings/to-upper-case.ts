55f9c570131fccc9d147474944701137cc72780a
dirgaprksha <dirga.prakesha@gmail.com>
2025-12-31 14:36:51 +0700

    feat: add new toUpperCase function and unit test

---
+++ packages/core/src/strings/to-upper-case.ts
@@ -0,0 +1,8 @@
+/**
+ * Converts a string to upper case.
+ * @param value - String value to upper case.
+ * @returns String value upper case.
+ */
+export function toUpperCase(value: string): string {
+  return value.toUpperCase()
+}
