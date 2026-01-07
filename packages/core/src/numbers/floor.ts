4a1d8ae97baa0646a65c797a3c4806e28a4138a8
dirgaprksha <dirga.prakesha@gmail.com>
2026-01-07 23:38:55 +0700

    feat: add new floor function and unit test

---
+++ packages/core/src/numbers/floor.ts
@@ -0,0 +1,8 @@
+/**
+ * Rounds number down to nearest integer.
+ * @param value - Number value to round down.
+ * @returns Number value rounded.
+ */
+export function floor(value: number): number {
+  return Math.floor(value)
+}
