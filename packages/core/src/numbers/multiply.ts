96ee35a63900d0974302bf7a347fc0e01eda10a0
dirgaprksha <dirga.prakesha@gmail.com>
2026-01-07 23:22:32 +0700

    feat: add new multiply function and unit test

---
+++ packages/core/src/numbers/multiply.ts
@@ -0,0 +1,9 @@
+/**
+ * Multiplies two numbers together.
+ * @param valueA - Number value to multiply.
+ * @param valueB - Number value to multiply.
+ * @returns Number value multipled.
+ */
+export function multiply(valueA: number, valueB: number): number {
+  return valueA * valueB
+}
