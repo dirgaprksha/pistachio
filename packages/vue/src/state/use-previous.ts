13ba1dc44c22330367c6db0e98a052134358f92b
dirgaprksha <dirga.prakesha@gmail.com>
2026-01-11 00:58:19 +0700

    feat: add new usePrevious function and unit test

---
+++ packages/vue/src/state/use-previous.ts
@@ -0,0 +1,23 @@
+import { isRef, shallowRef, watch } from 'vue'
+import type { Ref, ShallowRef } from 'vue'
+
+/**
+ * Vue composable to track previous value of a state or prop.
+ * @param value - Current value to track.
+ * @returns Previous value.
+ */
+export function usePrevious<T>(value: Ref<T> | T): ShallowRef<T | undefined> {
+  const previous = shallowRef<T | undefined>(undefined)
+  let currentValue = isRef(value) ? value.value : value
+
+  watch(
+    isRef(value) ? value : () => value,
+    (newValue) => {
+      previous.value = currentValue
+      currentValue = newValue
+    },
+    { flush: 'sync' },
+  )
+
+  return previous
+}
